87677b65282783d5f6a41237cb7e42992f33fb9c
JosEduardo93 <jchavez@fi.uba.ar>
2025-03-15 14:36:01 -0300

    Changes in .sh and tried passed test

--- generar-compose.sh
+++ generar-compose.sh
@@ -10,12 +10,12 @@ fi
 FILENAME=$1
 AMOUNT_CLIENTS=$2
 
-# if [ $AMOUNT_CLIENTS -lt 1 ]; then
-#     echo "Advertencia: La cantidad de clientes debe ser mayor o igual a 1. Se usará 1 como valor por defecto."
-#     AMOUNT_CLIENTS=1
-# fi
+if [ $AMOUNT_CLIENTS -lt 1 ]; then
+    echo "Advertencia: La cantidad de clientes debe ser mayor o igual a 1. Se usará 1 como valor por defecto."
+    AMOUNT_CLIENTS=1
+fi
 
 # Crear el archivo docker-compose.yaml
 echo "Generando archivo docker compose: $FILENAME con $AMOUNT_CLIENTS clientes..."
 python3 generator-compose.py $FILENAME $AMOUNT_CLIENTS
-# echo "Archivo docker-compose.yaml generado con exito"
+echo "Archivo docker-compose.yaml generado con exito"
